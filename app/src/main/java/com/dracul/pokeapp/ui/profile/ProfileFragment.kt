a8bbff3b2fac2111c4149771e67a0474f409fe25
dracul <apoapr738@gmail.com>
2024-05-09 17:14:23 +0300

    add ProfileFragment

---
+++ app/src/main/java/com/dracul/pokeapp/ui/profile/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.dracul.pokeapp.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dracul.pokeapp.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
