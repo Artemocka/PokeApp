b40d029df41bf9979581b8f330967c2c2fd523d9
dracul <apoapr738@gmail.com>
2024-05-09 17:58:35 +0300

    add PokemonData to models

---
+++ domain/src/main/java/com/example/domain/models/pokemondata/PokemonData.kt
@@ -0,0 +1,14 @@
+package com.example.domain.models.pokemondata
+
+import com.google.gson.annotations.SerializedName
+
+data class PokemonData(
+    @SerializedName("base_experience")
+    val baseExperience: Int,
+    val height: Int,
+    val id: Int,
+    val name: String,
+    val order: Int,
+    val sprites: Sprites,
+    val weight: Int
+)
